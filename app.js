11996065740b859cf4d1128ba06caa3072a8be18
Chris Wayne Comendador <cwayne.comendador@gmail.com>
2020-08-05 20:47:23 +0800

    add os module

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const log = require('./logger');
 const path = require('path');
+const os = require('os');
 
-let pathObj = path.parse(__filename);
+let totalMemory = os.totalmem();
+let freeMemory = os.freemem();
 
-console.log( pathObj );
+console.log(`TOTAL MEMORY: ${os.totalmem}`);
+console.log(`FREE MEMORY: ${os.freemem}`);
