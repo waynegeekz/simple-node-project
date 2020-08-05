128fe3b16d5316c0322040c253e0c3a40be80142
Chris Wayne Comendador <cwayne.comendador@gmail.com>
2020-08-05 20:59:26 +0800

    add Emitter module

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const log = require('./logger');
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
-fs.readdir('./', function(err, files) {
-    if (err) console.log('Error', err);
-    else console.log('Result', files);
+// Register a Listener
+emitter.on('messageLogged', function() {
+    console.log("Listener invoked!")
 });
+
+
+emitter.emit('messageLogged');
